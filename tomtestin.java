cc62fea47792978a2497d814a966a96df4d7e803
Farkas Csaba <fcsabi789@gmail.com>
2022-02-22 09:47:21 +0100

    kezdés

---
+++ tomtestin.java
@@ -0,0 +1,51 @@
+/*
+ * tomtestin.java
+ * 
+ * Copyright 2022 Diak <Diak@DESKTOP-Q6S7BVG>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+/*
+* File: tomtestin.java
+* Author: Farkas CSaba
+* Copyright: 2022, Farkas Csaba
+* Group: Szoft I/N
+* Date: 2022-02-22
+* Github: https://github.com/fcsabi/
+* Licenc: GNU GPL
+*/
+import java.util.Scanner;
+import java.io.*;
+
+public class tomtestin {
+	
+	public static void main (String[] args) {
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+
